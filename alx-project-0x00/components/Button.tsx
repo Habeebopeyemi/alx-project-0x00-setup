47a0c9bf4236c3cc9e5177fc3b81156b437f6b80
Habeebopeyemi <opeyemihabeeb4@gmail.com>
2025-07-14 14:51:46 +0100

    feat: task 6 updated

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import type { ButtonProps } from "@/interfaces";
 
-const Button = ({ title, styles }: ButtonProps) => {
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
   const { size = "medium", shape = "rounded-md" } = styles;
 
-  const sizeClass =
-    size === "small"
-      ? "px-3 py-1 text-sm"
-      : size === "large"
-      ? "px-6 py-3 text-lg"
-      : "px-4 py-2 text-base";
+  const sizeClass = {
+    small: "px-3 py-1 text-sm",
+    medium: "px-4 py-2 text-base",
+    large: "px-6 py-3 text-lg",
+  }[size];
 
   return (
     <button className={`bg-blue-500 text-white ${sizeClass} ${shape}`}>
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,23 @@
 import Card from "@/components/Card";
+import Button from "@/components/Button";
 
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
+      <Button
+        title="Small / Rounded-sm"
+        styles={{ size: "small", shape: "rounded-sm" }}
+      />
+      <Button
+        title="Medium / Rounded-md"
+        styles={{ size: "medium", shape: "rounded-md" }}
+      />
+      <Button
+        title="Large / Rounded-full"
+        styles={{ size: "large", shape: "rounded-full" }}
+      />
     </div>
   );
 };
